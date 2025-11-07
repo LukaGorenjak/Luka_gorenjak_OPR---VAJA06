2d84a9d75830753f59ea8302ac4b4b37bc281874
LukaGorenjak <121439233+LukaGorenjak@users.noreply.github.com>
2025-11-07 09:50:49 +0100

    Dodal komentarje razredu Igralec

--- Vaja6/Program.cs
+++ Vaja6/Program.cs
@@ -14,6 +14,7 @@ namespace vaja6 {
         public string u_geslo;
         public static int stIg = 1;
 
+		// Privzeti konstrukotor
         public Igralec() {
             u_ime = "";
             u_geslo = "";
@@ -21,6 +22,7 @@ namespace vaja6 {
             stIg++;
         }
 
+		// Lastnost ID
         public int Id {
             get
             {
@@ -28,6 +30,7 @@ namespace vaja6 {
             }
         }
 
+		// Lastnost ime
         public string U_ime {
             get
             {
@@ -39,6 +42,7 @@ namespace vaja6 {
             }
         }
 
+		// Lastnost geslo
         public string U_geslo
         {
             get
@@ -51,6 +55,7 @@ namespace vaja6 {
             }
         }
 
+		// Lastnost stevilo igralcev
         public static int StIg
         {
             get { return stIg; }
